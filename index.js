71745c5ebb05436f0523b6824201427472665995
Jay <jayshah8880@gmail.com>
2024-10-20 21:20:17 +0530

    intial commits for js files

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import * as core from '@actions/core';
+import * as core from '@actions/github';
 
-const nameToGreet = core.getInput('who-to-greet');
-const outputVal = `Hello ${nameToGreet}!`;
-core.setOutput('outputKey','outputVal')
+
+try {
+    const nameToGreet = core.getInput('who-to-greet');
+    const outputVal = `Hello ${nameToGreet}!`;
+    core.setOutput('outputKey','outputVal');
+} catch(error) {
+    core.setFailed(error.messsage)
+}
